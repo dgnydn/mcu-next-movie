eb3d719ad6a3810eaca55a8cc77388fcbe185c9f
Doğan Aydın <dogan1aydin@gmail.com>
2022-10-30 04:56:47 +0300

    font subset

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 import "../styles/globals.css";
 import { Inter } from "@next/font/google";
 
-const inter = Inter();
+const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
